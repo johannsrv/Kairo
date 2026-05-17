3446d8829ef476f3dce97460c0082a219085aa41
johannsrv <johann.rodriguezv@gmail.com>
2026-05-16 20:15:07 -0500

    Update package.xml and setup.py with proper descriptions, license, and file listings for Kairo robot simulation

--- src/my_robot_description/setup.py
+++ src/my_robot_description/setup.py
@@ -1,6 +1,15 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_robot_description'
+
+def list_files_recursively(directory):
+    files = []
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
 
 setup(
     name=package_name,
@@ -10,20 +19,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), list_files_recursively('urdf')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='johann',
     maintainer_email='johann.rodriguezv@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-        ],
-    },
+    description='Description of the Kairo robot for simulation in Gazebo',
+    license='Apache-2.0',
+    extras_require={'test': ['pytest']},
+    entry_points={'console_scripts': []},
 )
