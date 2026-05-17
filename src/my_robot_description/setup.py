baa5b2ab22c342cde7f697e4fde4a345b57c25c0
johannsrv <johann.rodriguezv@gmail.com>
2026-05-17 11:26:58 -0500

    Refactor setup.py to simplify file inclusion and improve package structure; remove unused recursive file listing function

--- src/my_robot_description/setup.py
+++ src/my_robot_description/setup.py
@@ -1,34 +1,36 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 import os
 from glob import glob
 
 package_name = 'my_robot_description'
 
-def list_files_recursively(directory):
-    files = []
-    for root, _, filenames in os.walk(directory):
-        for filename in filenames:
-            files.append(os.path.join(root, filename))
-    return files
-
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Launch files
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'urdf'), list_files_recursively('urdf')),
-        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        # Config files (controllers.yaml)
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        # URDF xacro files
+        (os.path.join('share', package_name, 'urdf', 'urdf'), glob('urdf/urdf/*.xacro')),
+        # STL files (opcional, si quieres tenerlos en install)
+        (os.path.join('share', package_name, 'urdf', 'stl', 'electronics'), glob('urdf/stl/electronics/*.stl')),
+        (os.path.join('share', package_name, 'urdf', 'stl', 'robot_body'), glob('urdf/stl/robot_body/*.stl')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='johann',
-    maintainer_email='johann.rodriguezv@gmail.com',
-    description='Description of the Kairo robot for simulation in Gazebo',
-    license='Apache-2.0',
-    extras_require={'test': ['pytest']},
-    entry_points={'console_scripts': []},
+    maintainer_email='johann@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
 )
