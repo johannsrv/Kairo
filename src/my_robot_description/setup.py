d4758618dae5cee284d07de039d5378ddb9453bb
johannsrv <johann.rodriguezv@gmail.com>
2026-05-19 13:39:57 -0500

    Add WoodenChair model and associated assets
    - Created new model for WoodenChair including .obj and .mtl files for geometry and materials.
    - Added texture images: WoodenChair_Diffuse.png and WoodenChair_Rough.png.
    - Included model configuration (model.config) and SDF file (model.sdf) for integration with simulation environments.
    - Updated world file (world_restauran.sdf) to include WoodenChair instances.
    - Added thumbnail images for the WoodenChair model.
    - Enhanced setup.py to include new model files in the package installation.

--- src/my_robot_description/setup.py
+++ src/my_robot_description/setup.py
@@ -4,34 +4,106 @@ from glob import glob
 
 package_name = 'my_robot_description'
 
+
+def package_files(directory):
+    paths = []
+
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join(path, filename))
+
+    return paths
+
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Launch files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # Config files (controllers.yaml)
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        # URDF xacro files
-        (os.path.join('share', package_name, 'urdf', 'urdf'), glob('urdf/urdf/*.xacro')),
-        # STL files (opcional, si quieres tenerlos en install)
-        (os.path.join('share', package_name, 'urdf', 'stl', 'electronics'), glob('urdf/stl/electronics/*.stl')),
-        (os.path.join('share', package_name, 'urdf', 'stl', 'robot_body'), glob('urdf/stl/robot_body/*.stl')),
-        (os.path.join('share', package_name, 'world'),glob('world/*.sdf')),
+
+        # Ament
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name]
+        ),
+
+        # package.xml
+        (
+            'share/' + package_name,
+            ['package.xml']
+        ),
+
+        # Launch
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')
+        ),
+
+        # Config
+        (
+            os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')
+        ),
+
+        # Worlds
+        (
+            os.path.join('share', package_name, 'world'),
+            glob('world/*.sdf')
+        ),
+
+        # URDF/Xacro
+        (
+            os.path.join('share', package_name, 'urdf', 'urdf'),
+            glob('urdf/urdf/*.xacro')
+        ),
+
+        # STL electronics
+        (
+            os.path.join(
+                'share',
+                package_name,
+                'urdf',
+                'stl',
+                'electronics'
+            ),
+            glob('urdf/stl/electronics/*.stl')
+        ),
+
+        # STL robot body
+        (
+            os.path.join(
+                'share',
+                package_name,
+                'urdf',
+                'stl',
+                'robot_body'
+            ),
+            glob('urdf/stl/robot_body/*.stl')
+        ),
+
+    ] +
+
+    # MODELS COMPLETOS
+    [
+        (
+            os.path.join(
+                'share',
+                package_name,
+                os.path.dirname(path)
+            ),
+            [path]
+        )
+        for path in package_files('models')
     ],
+
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='johann',
     maintainer_email='johann@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Robot description package',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
